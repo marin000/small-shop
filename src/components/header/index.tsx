a00729eec8ec9012fb07ba34cd75f9f064562a5c
marin000 <marinb997@gmail.com>
2024-12-11 01:08:19 +0100

    add logo, favicon, create header

---
+++ src/components/header/index.tsx
@@ -0,0 +1,19 @@
+import { Link } from 'react-router-dom';
+import logo from '@/assets/images/logo.png';
+
+const Header = () => {
+  return (
+    <header className="z-40 py-2 px-12 bg-black flex items-center justify-center">
+      <h1 className="flex items-center space-x-4">
+        <Link to="/" className="flex items-center">
+          <img src={logo} alt="app logo" className="w-20 h-12" />
+          <span className="text-white text-2xl font-semibold ml-4">
+            Grab & Go
+          </span>
+        </Link>
+      </h1>
+    </header>
+  );
+};
+
+export default Header;
